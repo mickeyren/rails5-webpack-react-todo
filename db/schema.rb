b4ac22bea2f29969f9d22269ad2d6efea407ecd9
David Ang <davidang09@gmail.com>
2017-08-15 05:46:28 +0800

    i think notes should be pluralize
    also accidentally named the db with a "db/", it comes with it from the initial rails project

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20170814135900) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "note"
+    t.text "notes"
     t.date "completed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
